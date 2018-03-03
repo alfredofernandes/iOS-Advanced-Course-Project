05b76c3a1937976063e6d4e5b63dc949b377df5d
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2018-03-03 17:57:50 -0500

    Logo and description properties added to company model

--- Advanced-Course-Project/Model/Company.swift
+++ Advanced-Course-Project/Model/Company.swift
@@ -8,28 +8,30 @@
 
 import ObjectMapper
 
-class Company: Mappable {
+public class Company: Mappable {
     
     private var jobs: [Job]?
     private var name: String?
+    private var logo: String?
     private var size: String?
-    private var photo: String?
     private var contact: String?
     private var website: String?
     private var techStack: String?
+    private var description: String?
     
-    required convenience init?(map: Map) {
+    public required convenience init?(map: Map) {
         self.init()
     }
     
-    func mapping(map: Map) {
+    public func mapping(map: Map) {
         jobs        <- map["jobs"]
         name        <- map["name"]
+        logo        <- map["logo"]
         size        <- map["size"]
-        photo       <- map["photo"]
         contact     <- map["contact"]
         website     <- map["website"]
         techStack   <- map["techStack"]
+        description <- map["description"]
     }
     
 }
