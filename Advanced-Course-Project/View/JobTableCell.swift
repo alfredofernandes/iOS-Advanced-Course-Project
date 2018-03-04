e2568de47708921562e0e952e2f64749ff266ba0
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-03-04 09:45:35 -0500

    Create Job List View with a table

---
+++ Advanced-Course-Project/View/JobTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobTableCell.swift
+//  Advanced-Course-Project
+//
+//  Created by Araceli Teixeira on 04/03/18.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import UIKit
+
+class JobTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
