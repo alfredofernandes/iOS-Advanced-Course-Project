97551d40d8acde4031e3337674847086e370ce67
Alfredo Fernandes <alfredorfernandes@gmail.com>
2018-03-03 20:05:01 -0500

    Update Job Model

--- Advanced-Course-Project/Model/Job.swift
+++ Advanced-Course-Project/Model/Job.swift
@@ -2,12 +2,32 @@
 //  Job.swift
 //  Advanced-Course-Project
 //
-//  Created by Alfredo Fernandes on 2018-03-03.
+//  Created by Guilherme Crozariol on 2018-03-01.
 //  Copyright © 2018 ProDigi-Development. All rights reserved.
 //
 
-import Foundation
+import ObjectMapper
 
-class Job {
+class Job: Mappable {
+    
+    private var title: String?
+    private var salary: String?
+    private var benefits: String?
+    private var startDate: String?
+    private var postingDate: String?
+    private var description: String?
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        title       <- map["title"]
+        description <- map["description"]
+        startDate   <- map["startDate"]
+        postingDate <- map["postingDate"]
+        salary      <- map["salary"]
+        benefits    <- map["benefits"]
+    }
     
 }
