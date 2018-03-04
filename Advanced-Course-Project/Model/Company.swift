07cb88a498d31cd9d354607c505f917675751158
Alfredo Fernandes <alfredorfernandes@gmail.com>
2018-03-03 19:50:49 -0500

    Update company model

--- Advanced-Course-Project/Model/Company.swift
+++ Advanced-Course-Project/Model/Company.swift
@@ -6,8 +6,35 @@
 //  Copyright © 2018 ProDigi-Development. All rights reserved.
 //
 
-import Foundation
+import ObjectMapper
 
 class Company {
     
+    private var name: String?
+    private var address: String?
+    private var website: String?
+    private var email: String?
+    private var phone: String?
+    private var history: String?
+    private var product: String?
+    private var techStack: String?
+    private var sizeCompany: String?
+    private var rating: Int?
+    
+    public required convenience init(map: Map) {
+        self.init()
+    }
+    
+    public func mapping(map: Map) {
+        name        <- map["name"]
+        address     <- map["address"]
+        website     <- map["website"]
+        email       <- map["email"]
+        phone       <- map["phone"]
+        history     <- map["history"]
+        product     <- map["product"]
+        techStack   <- map["techStack"]
+        sizeCompany <- map["sizeCompany"]
+        rating      <- map["rating"]
+    }
 }
