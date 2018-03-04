22b0e63808f8f518c1beb4dcb95463468292487a
Alfredo Fernandes <alfredorfernandes@gmail.com>
2018-03-03 19:11:43 -0500

    Create Job Model

---
+++ Advanced-Course-Project/Model/Job.swift
@@ -0,0 +1,13 @@
+//
+//  Job.swift
+//  Advanced-Course-Project
+//
+//  Created by Alfredo Fernandes on 2018-03-03.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import Foundation
+
+class Job {
+    
+}
