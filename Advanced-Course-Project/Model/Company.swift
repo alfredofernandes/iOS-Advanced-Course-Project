19b39b2b658a10a49741a8a20b0c20e8b604ab26
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-03-06 18:54:04 -0500

    Add new constructor and set as private the default constructor

--- Advanced-Course-Project/Model/Company.swift
+++ Advanced-Course-Project/Model/Company.swift
@@ -12,6 +12,7 @@ public class Company: Mappable {
     
     public private(set) var jobs: [Job]?
     public private(set) var name: String?
+    public private(set) var email: String?
     public private(set) var size: String?
     public private(set) var photo: String?
     public private(set) var contact: String?
@@ -22,41 +23,24 @@ public class Company: Mappable {
         self.init()
     }
     
+    private init() {
+        // Do NOT use this constructor
+    }
+    
+    // Constructor for minimal object
+    internal init(withName name: String, andEmail email: String) {
+        self.name = name
+        self.email = email
+    }
+    
     public func mapping(map: Map) {
         jobs        <- map["jobs"]
         name        <- map["name"]
+        email       <- map["email"]
         size        <- map["size"]
         photo       <- map["photo"]
         contact     <- map["contact"]
         website     <- map["website"]
         techStack   <- map["techStack"]
-    }
-    
-    public func setJobs(_ jobs: [Job]) {
-        self.jobs = jobs
-    }
-    
-    public func setName(_ name: String) {
-        self.name = name
-    }
-    
-    public func setSize(_ size: String) {
-        self.size = size
-    }
-    
-    public func setPhoto(_ photo: String) {
-        self.photo = photo
-    }
-    
-    public func setContact(_ contact: String) {
-        self.contact = contact
-    }
-    
-    public func setWebsite(_ website: String) {
-        self.website = website
-    }
-    
-    public func setTechStack(_ techStack: String) {
-        self.techStack = techStack
     }
 }
