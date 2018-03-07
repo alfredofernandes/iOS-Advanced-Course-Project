817ebdeaa141de57588e1a2d480c43707ff13042
Caio Dias <kiobreno@gmail.com>
2018-03-06 19:26:39 -0500

    Add listCompanies method with stub result

--- Advanced-Course-Project/Controller/CompanyController.swift
+++ Advanced-Course-Project/Controller/CompanyController.swift
@@ -9,5 +9,26 @@
 import Foundation
 
 public class CompanyController {
+    public static let shared: CompanyController = CompanyController()
     
+    private init() {
+        // Nothing :)
+    }
+    
+    public func listCompanies() -> [Company] {
+        return generateStubCompanies()
+    }
+    
+    private func generateStubCompanies() -> [Company] {
+        var stubCompanies = [Company]()
+        let randomInt = Int(arc4random_uniform(16))
+        
+        for num in 0...randomInt {
+            let company = Company(withName: "\(num)", andEmail: "\(num)@email.com")
+
+            stubCompanies.append(company)
+        }
+        
+        return stubCompanies
+    }
 }
