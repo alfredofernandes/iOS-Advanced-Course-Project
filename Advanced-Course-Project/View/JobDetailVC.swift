e9b08230395efe8b5817b6d0778aee34bbef4b56
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2018-03-06 19:34:10 -0500

    Create jobDetailVC

---
+++ Advanced-Course-Project/View/JobDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  JobDetailVC.swift
+//  Advanced-Course-Project
+//
+//  Created by Guilherme Crozariol on 2018-03-06.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import UIKit
+
+class JobDetailVC: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var salaryLabel: UILabel!
+    @IBOutlet weak var benefitsLabel: UILabel!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var postingDateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
