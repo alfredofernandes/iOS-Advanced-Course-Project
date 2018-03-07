80d9b6caa67ffb72df3dd75d47cce375746dbe8b
Caio Dias <kiobreno@gmail.com>
2018-03-06 19:11:18 -0500

    Add CompanyController class

---
+++ Advanced-Course-Project/Controller/CompanyController.swift
@@ -0,0 +1,13 @@
+//
+//  CompanyController.swift
+//  Advanced-Course-Project
+//
+//  Created by Caio Dias on 2018-03-06.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import Foundation
+
+public class CompanyController {
+    
+}
