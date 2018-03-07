93fd89a69dff53ab02e622afa23492cbdea5025f
Alfredo Fernandes <alfredorfernandes@gmail.com>
2018-03-06 19:19:25 -0500

    Create FetchController

---
+++ Advanced-Course-Project/Model/Fetch/FetchController.swift
@@ -0,0 +1,13 @@
+//
+//  FetchController.swift
+//  Advanced-Course-Project
+//
+//  Created by Alfredo Fernandes on 2018-03-06.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import Foundation
+
+internal final class FetchController {
+    
+}
