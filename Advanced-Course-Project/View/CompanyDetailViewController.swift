ada5ff4c1ccd6d62efba72696de89688a3d6791b
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2018-03-09 07:32:13 -0500

    Update labels on ViewDidAppear in CompanyDetailViewController

--- Advanced-Course-Project/View/CompanyDetailViewController.swift
+++ Advanced-Course-Project/View/CompanyDetailViewController.swift
@@ -20,4 +20,20 @@ class CompanyDetailViewController: UIViewController {
     @IBOutlet weak var techStackLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let company = CoreFacade.shared.getCompany()
+        
+        nameLabel.text = company.name
+        emailLabel.text = company.email
+        logoLabel.text = "This is a logo"
+        sizeLabel.text = "1000+"
+        photoLabel.text = "This is a photo"
+        contactLabel.text = "647-675-3412"
+        websiteLabel.text = "https://www.gcrozariol.com/"
+        techStackLabel.text = "IT"
+        descriptionLabel.text = "This company has been developing apps..."
+    }
+    
 }
