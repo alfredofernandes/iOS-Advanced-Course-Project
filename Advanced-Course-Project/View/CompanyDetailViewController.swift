0f1fdc02757ab198a8688f4969fd5cc11d2d691f
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2018-03-09 06:51:00 -0500

    Create CompanyDetailViewController

---
+++ Advanced-Course-Project/View/CompanyDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CompanyDetailViewController.swift
+//  Advanced-Course-Project
+//
+//  Created by Guilherme Crozariol on 2018-03-09.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import UIKit
+
+class CompanyDetailViewController: UIViewController {
+    
+    
+    
+}
