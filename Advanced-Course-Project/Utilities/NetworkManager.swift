c44390dc06e221b8f900929679050f5fbcd4e835
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2018-03-13 19:31:06 -0400

    Create NetworkManager class

---
+++ Advanced-Course-Project/Utilities/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  Advanced-Course-Project
+//
+//  Created by Guilherme Crozariol on 2018-03-13.
+//  Copyright © 2018 ProDigi-Development. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+class NetworkManager {
+    
+    
+    
+}
